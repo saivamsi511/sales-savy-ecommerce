a14bf5ccf095c020b9210bb06056964fe9268a91
saivamsi511 <battasaivamsi123@gmail.com>
2025-09-02 18:31:25 +0530

    First commit

---
+++ Sales_savy_full_stack_app/src/main/java/com/example/demo/SalesSavyFullStackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSavyFullStackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSavyFullStackAppApplication.class, args);
+	}
+
+}
---
+++ Sales_savy_full_stack_app/src/test/java/com/example/demo/SalesSavyFullStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSavyFullStackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
